40d813752dce64e3d977b40cbf0cf6e68a8ab035
Senlovan <maiconmattossenlovan@gmail.com>
2023-04-28 21:03:42 -0300

    Subindo Aula 5

---
+++ index.js
@@ -0,0 +1,45 @@
+//const numeros = [14, 242, 673, 534]
+//console.log(numeros)
+
+//const numeros = [14, 242, 673, 534]
+//console.log(numeros [1])
+
+//const raçasCachorros = ["Rotvwailler", "Pastor" , "Shitsu", "Poodle" , "Caramelo"]
+//console.log(raçasCachorros.includes("Caramelo"));
+
+//console.log (raçasCachorros.length)
+
+
+//const raçasCachorros2 = Number (prompt ("Escolha um número de 0 a 4"))
+
+
+/*raçasCachorros[raçasCachorros2] === undefined ?
+console.log ("Você deve informar numeros entre 0 e 4"):
+console.log(raçasCachorros[raçasCachorros2]);*/
+
+/*const dog = ["caramelo", "vira lata","husky"]
+dog.splice(1,1)
+console.log (dog)*/
+
+const numeros = [1,2,3,4,5,6]
+console.log(numeros.length);
+numeros.push(7)
+numeros.splice(3,2)
+console.log (numeros.length)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//dog.pop()
+
+
